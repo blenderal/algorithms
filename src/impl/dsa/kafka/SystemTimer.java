d6afc93bf3f25e8b1e44b591c33d009a4b5770ab
stomach <zwwzjjs@gmail.com>
2020-11-06 19:53:50 +0800

    时间轮

---
+++ src/impl/dsa/kafka/SystemTimer.java
@@ -0,0 +1,72 @@
+package impl.dsa.kafka;
+
+import java.util.concurrent.*;
+
+/**
+ * @Author: siran.yao
+ * @time: 2020/5/8:下午1:13
+ * 对时间轮的包装
+ */
+public class SystemTimer {
+    /**
+     * 底层时间轮
+     */
+    private TimingWheel timeWheel;
+
+    /**
+     * 一个Timer只有一个delayQueue
+     */
+    private DelayQueue<TimerTaskList> delayQueue = new DelayQueue<>();
+
+    /**
+     * 过期任务执行线程
+     */
+    private ExecutorService workerThreadPool;
+
+    /**
+     * 轮询delayQueue获取过期任务线程
+     */
+    private ExecutorService bossThreadPool;
+
+    /**
+     * 构造函数
+     */
+    public SystemTimer() {
+        timeWheel = new TimingWheel(1, 20, System.currentTimeMillis(), delayQueue);
+        workerThreadPool = Executors.newFixedThreadPool(100);
+        bossThreadPool = Executors.newFixedThreadPool(1);
+        //20ms获取一次过期任务
+        bossThreadPool.submit(() -> {
+            while (true) {
+                this.advanceClock(20);
+            }
+        });
+    }
+
+    /**
+     * 添加任务
+     */
+    public void addTask(TimerTask timerTask) {
+        //添加失败任务直接执行
+        if (!timeWheel.addTask(timerTask)) {
+            workerThreadPool.submit(timerTask.getTask());
+        }
+    }
+
+    /**
+     * 获取过期任务
+     */
+    private void advanceClock(long timeout) {
+        try {
+            TimerTaskList timerTaskList = delayQueue.poll(timeout, TimeUnit.MILLISECONDS);
+            if (timerTaskList != null) {
+                //推进时间
+                timeWheel.advanceClock(timerTaskList.getExpiration());
+                //执行过期任务（包含降级操作）
+                timerTaskList.flush(this::addTask);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/impl/dsa/kafka/TimerTask.java
@@ -0,0 +1,58 @@
+package impl.dsa.kafka;
+
+/**
+ * @Author: siran.yao
+ * @time: 2020/5/8:上午11:13
+ */
+public class TimerTask{
+    /**
+     * 延迟时间
+     */
+    private long delayMs;
+
+    /**
+     * 任务
+     */
+    private Runnable task;
+
+    /**
+     * 时间槽
+     */
+    protected TimerTaskList timerTaskList;
+
+    /**
+     * 下一个节点
+     */
+    protected TimerTask next;
+
+    /**
+     * 上一个节点
+     */
+    protected TimerTask prev;
+
+    /**
+     * 描述
+     */
+    public String desc;
+
+    public TimerTask(Runnable task,long delayMs) {
+        this.delayMs = System.currentTimeMillis() + delayMs;
+        this.task = task;
+        this.timerTaskList = null;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public Runnable getTask() {
+        return task;
+    }
+
+    public long getDelayMs() {
+        return delayMs;
+    }
+
+    @Override
+    public String toString() {
+        return desc;
+    }
+}
